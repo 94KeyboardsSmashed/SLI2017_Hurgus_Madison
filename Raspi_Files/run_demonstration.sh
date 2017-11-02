be3188ef3c68bd168b555e3c198e82461006f893
94KeyboardsSmashed <hyunseok1122@gmail.com>
2017-11-01 22:57:49 -0500

    added startup capability

--- Raspi_Files/run_demonstration.sh
+++ Raspi_Files/run_demonstration.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Run this script when using websockets
-sudo nice -20 python3 raspi_quiet_ex.py > log.txt & websocketd --port=8080 --staticdir=./static tail -f log.txt
+sudo nice -20 python3 /home/pi/SLI2017_Hurgus_Madison/Raspi_Files/raspi_run2.py > /home/pi/SLI2017_Hurgus_Madison/Raspi_Files/log.txt & websocketd --port=8080 --staticdir=/home/pi/SLI2017_Hurgus_Madison/Raspi_Files/static tail -f /home/pi/SLI2017_Hurgus_Madison/Raspi_Files/log.txt
